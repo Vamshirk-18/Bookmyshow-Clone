98310f319798690a63fe1686fa23530c3919782f
Vamshirk-18 <vamshirk05@gmail.com>
2025-03-30 15:55:12 +0530

    Update Sports.js

--- src/components/Sports.js
+++ src/components/Sports.js
@@ -5,7 +5,7 @@ const Sports = () => {
   return (
     <Container className="mt-4 text-center">
       <h2>Sports Section</h2>
-      <p className="lead">Exciting Sports Events Coming Soon!!!! Stay Tuned.</p>
+      <p className="lead">Exciting Sports Events Coming Soon!!!! Stay Tuned!!</p>
     </Container>
   );
 };
